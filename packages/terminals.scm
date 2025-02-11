c01edfb0e1077502568e2d5b2bbb01290306d775
Alex Bosco <ajbosco00@gmail.com>
2025-02-11 12:59:42 -0500

    test

--- packages/terminals.scm
+++ packages/terminals.scm
@@ -20,8 +20,7 @@
 	      "18i499hhxly1r2bnqp9wssh0p1v391cxf10aydxaa7mdmrd3vqh9"))))
    (build-system copy-build-system)
    (arguments
-    `(#:install-plan `(("*" "/.tmux/plugins/tpm"))))
+    `(#:install-plan `(("." "share/.tmux/plugins/tpm"))))
    (synopsis "A tmux plugin manager.")
-   (description "To make use of this derivation, use
-		\"run\" \"'~/.tmux/plugins/tpm/tpm'\"")
+   (description "To make use of this derivation, use")
    (license license:expat)))
