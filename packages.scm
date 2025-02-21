df603c08af197ccbf455261201bde5f6098ac03b
Alex Bosco <ajbosco00@gmail.com>
2025-02-21 18:21:54 -0500

    test

--- packages.scm
+++ packages.scm
@@ -130,25 +130,25 @@
       (specification->package pkg)
       (specification->package "curl")))
 
-(define-public font-monaspace
-  (package
-    (name "font-monaspace")
-    (version "1.101")
-    (source
-     (origin
-       (method url-fetch)
-       (uri
-        "https://github.com/githubnext/monaspace/archive/refs/tags/v1.101.tar.gz")
-       (sha256
-        (base32 "076gx85and4xb262y0rbqvy7f6w732krzlh236xr7v3zbsw1h872"))))
-    (build-system font-build-system)
-    (home-page "https://monaspace.githubnext.com")
-    (synopsis "An innovative superfamily of fonts for code")
-    (description
-     "The Monaspace type system is a monospaced type superfamily with some modern tricks up its sleeve.
-     It consists of five variable axis typefaces. Each one has a distinct voice, but they are all metrics-compatible with one another,
-     allowing you to mix and match them for a more expressive typographical palette.")
-    (license license:silofl1.1)))
+; (define-public font-monaspace
+;   (package
+;     (name "font-monaspace")
+;     (version "1.101")
+;     (source
+;      (origin
+;        (method url-fetch)
+;        (uri
+;         "https://github.com/githubnext/monaspace/archive/refs/tags/v1.101.tar.gz")
+;        (sha256
+;         (base32 "076gx85and4xb262y0rbqvy7f6w732krzlh236xr7v3zbsw1h872"))))
+;     (build-system font-build-system)
+;     (home-page "https://monaspace.githubnext.com")
+;     (synopsis "An innovative superfamily of fonts for code")
+;     (description
+;      "The Monaspace type system is a monospaced type superfamily with some modern tricks up its sleeve.
+;      It consists of five variable axis typefaces. Each one has a distinct voice, but they are all metrics-compatible with one another,
+;      allowing you to mix and match them for a more expressive typographical palette.")
+;     (license license:silofl1.1)))
 
 ;; TODO install to right location where Kvantum can see it?
 (define-public kv-yaru-colors
